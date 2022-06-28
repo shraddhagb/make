5e7661f6d7076a408232b5e54c8fa47d9824ad2d
shraddhagb <106031133+shraddhagb@users.noreply.github.com>
2022-06-28 13:07:34 +0530

    Create sum2.c

---
+++ sum2.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+void sum2() {
+  int num1, num2, sum;
+  printf("Enter two integers:  ");
+  scanf(%d %d, &num1, &num2);
+  sum = num1 + num2;
+  printf("%d + %d = %d" num1, num2, sum)
